9dcee82c756533c4fc519f90c03db9a0f148a7eb
Urusha <urusha10@gmail.com>
2024-02-04 12:48:08 -0500

    week 5 first question

--- basics/week_5.py
+++ basics/week_5.py
@@ -1,4 +1,54 @@
 """
+You are given a 0-indexed binary string s having an even length.
+
+A string is beautiful if it's possible to partition it into one or more substrings such that:
+
+Each substring has an even length.
+Each substring contains only 1's or only 0's.
+You can change any character in s to 0 or 1.
+
+Return the minimum number of changes required to make the string s beautiful.
+"""
+
+s='10'
+beautiful = False
+output=0
+
+def check_beautiful(s):
+    if(len(s)==2):
+        if s[0]!=s[1]:
+            return False
+    else:
+        for i in range (1, len(s)):
+            if s[i] != s[0]:
+                print(s[i])
+                return False
+    return True
+    
+
+
+def make_beautiful(s,output):
+    if(len(s))<=2:
+        output +=1
+    else:
+        for i in range(0,len(s)-1,2):
+            if(s[i]!=s[i+1]):
+                output += 1
+    return (output)
+    
+
+        
+if not check_beautiful(s):
+    print(f'Output: {make_beautiful(s,output)}')
+else:
+    print(f'Output: {output}')
+    
+        
+
+
+        
+    
+"""
 Write a Python program to find all the numbers from 0-9 from a string:
 
 input: '89ADFRE41'
